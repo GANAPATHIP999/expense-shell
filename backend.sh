2224c1062ac75a4f816fda988822211b7ec93b2d
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-17 11:38:48 -0500

    function

--- backend.sh
+++ backend.sh
@@ -67,23 +67,3 @@ VALIDATE $? "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "Copied backend service"
-
-systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon-reload"
-
-systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
-
-systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
-
-dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Client"
-
-mysql -h db.aws-9s.shop -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema Loading"
-
-systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restarting backend"
